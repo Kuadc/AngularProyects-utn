fba3e5e45a05daeb7709c25caf7b2a043eb88bb2
Carlos Catalan <contactocatalan@gmail.com>
2024-12-13 12:57:36 -0300

    practico1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'practico1';
+  name = "Carlos Catalan";
+  inputname= "new name";
+  changename(newname:string, inputElement: HTMLInputElement)
+  {
+    if (newname){
+      this.name = newname; 
+    inputElement.value = "";
+    }
+    else{
+      alert("Enter a new name");
+    }
+    
+  }
 }
